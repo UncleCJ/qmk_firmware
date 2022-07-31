4c5fb8bec843843f91010f1590fef22c6863d90e
CJ Sveningsson <cj.sveningsson@gmail.com>
2022-07-31 22:48:55 +0200

    Again, implement my preferences
    I did not learn yet why I could not add these in custom_config.h . Also tab on the first, left thumb key keeps tripping me up, though obviously Miryoku feels so much more thought through than any crkbd default.
    
    What is the deal with undef TAPPING_TERM?
    
    Configure as default by:
    
        qmk config user.keyboard=crkbd/rev1
        qmk config user.keymap=manna-harbour_miryoku

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,11 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS = QWERTY
+MIRYOKU_TAP = QWERTY
+MIRYOKU_EXTRA = COLEMAKDH
+MIRYOKU_NAV = VI
+FORCE_LAYOUT = split_3x6_3
+
+RGBLIGHT_ENABLE = yes
+OLED_ENABLE = yes
