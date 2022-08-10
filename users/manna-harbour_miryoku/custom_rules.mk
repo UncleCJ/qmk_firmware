1131d04f89ea3d5eb2d2ecf79cf946c44efb6653
Carl-Johan Sveningsson <cj.sveningsson@gmail.com>
2022-08-10 10:16:25 +0200

    WIP: Add swedish mapping
    In order for this to work, keycodes must be the appropriate (regular?) QWERTY codes, including sixth columns. See:
    
    * https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_swedish.h
    * https://www.reddit.com/r/MechanicalKeyboards/comments/f67mil/changing_to_swedish_layout_in_qmk_firmware

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,8 +1,8 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-MIRYOKU_ALPHAS = QWERTY
-MIRYOKU_TAP = QWERTY
+MIRYOKU_ALPHAS = QWERTY_SV
+MIRYOKU_TAP = QWERTY_SV
 MIRYOKU_EXTRA = COLEMAKDH
 MIRYOKU_NAV = VI
 FORCE_LAYOUT = split_3x6_3
